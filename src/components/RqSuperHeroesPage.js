27351bd5c2f0fdc707535d67bcab3e12376169d2
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 22:54:01 +0300

    Add React Query hooks and fetch superheroes data in RqSuperHeroesPage.js

--- src/components/RqSuperHeroesPage.js
+++ src/components/RqSuperHeroesPage.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const RqSuperHeroesPage = () => {
-  return <div>RqSuperHeroesPage</div>;
+  const { data, isLoading, error } = useQuery("super-heroes", async () => {
+    const { data } = await axios.get("http://localhost:5000/superheroes");
+    console.log(data);
+    return data;
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Super Heroes</h1>
+      <ul>
+        {data.map((hero) => (
+          <li key={hero.id}>
+            <h2>{hero.name}</h2>
+            <p>{hero.power}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RqSuperHeroesPage;
