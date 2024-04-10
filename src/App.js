f4fcc0375792625e718b834b0c6b8a160ab6e346
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 22:50:07 +0300

    Add react-query dependency and update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import SuperHeroes from "./components/SuperHeroes.page";
 import RqSuperHeroesPage from "./components/RqSuperHeroesPage";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="super-heroes" element={<SuperHeroes />} />
-        <Route path="rq-super-heroes" element={<RqSuperHeroesPage />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      {" "}
+      {/* Wrap your App with QueryClientProvider */}
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="super-heroes" element={<SuperHeroes />} />
+          <Route path="rq-super-heroes" element={<RqSuperHeroesPage />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
