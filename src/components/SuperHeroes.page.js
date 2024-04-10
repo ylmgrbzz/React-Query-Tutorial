ba22470b9f2aecd1be35d597eb69102053ace9c6
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 22:45:12 +0300

    Add axios dependency and update SuperHeroes.page.js

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SuperHeroes = () => {
-  return <div>SuperHeroesPage</div>;
+  const [isloading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/superheroes")
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isloading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Super Heroes</h1>
+      <ul>
+        {data.map((hero) => (
+          <li key={hero.id}>
+            <h2>{hero.name}</h2>
+            <p>{hero.power}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SuperHeroes;
