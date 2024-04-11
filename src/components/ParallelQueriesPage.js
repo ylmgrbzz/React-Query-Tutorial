df7165308907503b16790ed5bc69c2b480249e82
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-11 17:30:48 +0300

    Update ParallelQueriesPage component to use react-query for fetching data

--- src/components/ParallelQueriesPage.js
+++ src/components/ParallelQueriesPage.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+const fetchFriends = () => {
+  return axios.get("http://localhost:4000/friends");
+};
 
 const ParallelQueriesPage = () => {
-  return <div>ParallelQueriesPage</div>;
+  const { data: superHeroes } = useQuery("super-heroes", fetchSuperHeroes);
+  const { data: friends } = useQuery("friends", fetchFriends);
+  console.log(superHeroes, friends);
+  return <div>Parallel Queries</div>;
 };
 
 export default ParallelQueriesPage;
